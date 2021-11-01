ddda3059d03c583d620bee4372bf1a21d6603ffe
Walodja1987 <wladimir.weinbender@gmail.com>
2021-11-01 10:04:35 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import WalletConnectButton from './WalletConnectButton';
+
+function App() {
+  return (
+    <div className="App">
+      <WalletConnectButton />
+    </div>
+  );
+}
+
+export default App;
